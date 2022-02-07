b154fe3a3b4d460ed7375d3b81193c8ad7c370c1
Geoffroy d'Estaintot <geoffroy.destaintot@openergy.fr>
2022-02-07 15:41:22 +0100

    * m: api is now available at package's top level

--- RELEASE.md
+++ RELEASE.md
@@ -1,6 +1,7 @@
 # oazure
 
-## next
+## 1.2.0
+* m: api is now available at package's top level
 
 ## 1.1.0
 * m: logging_handler was added
---
+++ oazure/__init__.py
@@ -0,0 +1,7 @@
+from .async_blob_storage import AsyncBlobAPI, \
+    AzureBlobStorageResourceNotFound, \
+    AzureBlobStorageAlreadyLeased,\
+    AzureBlobStorageAlreadyReleased, \
+    AzureBlobStorageLockedFile, \
+    AzureBlobStorageAsyncError
+from .logging_handler import AzureLoggingHandler
